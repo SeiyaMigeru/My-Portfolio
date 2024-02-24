9af8f5c64aeca03992017143227c40e021d792be
Seiya <daigurenhyourinmaru20@gmail.com>
2024-02-24 12:24:35 +0800

    Final Commit (Header Fix)

--- components/header.tsx
+++ components/header.tsx
@@ -40,7 +40,7 @@ export default function Header() {
                 className={clsx(
                   "flex w-full items-center justify-center px-3 py-3 hover:text-slate-950 transition dark:text-slate-500 dark:hover:text-slate-200",
                   {
-                    "text-slate-950 dark:text-slate-200":
+                    "text-slate-950 dark:text-slate-100":
                       activeSection === link.name,
                   }
                 )}
